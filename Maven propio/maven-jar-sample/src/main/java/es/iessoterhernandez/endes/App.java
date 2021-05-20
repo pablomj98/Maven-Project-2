7f63ff25f25caa437080db3d478d6078cd59f80f
pablomj98 <74272274+pablomj98@users.noreply.github.com>
2021-05-20 12:29:04 +0200

    Add files via upload

---
+++ Maven propio/maven-jar-sample/src/main/java/es/iessoterhernandez/endes/App.java
@@ -0,0 +1,24 @@
+package es.iessoterhernandez.endes;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int saludo = 2;
+    	switch(saludo) {
+        case 1 :
+        	System.out.println( "Hello friend" );
+            break;
+        case 2:
+        	System.out.println( "Hola amigo" );
+            break;
+        case 3:
+        	System.out.println( "Salut mon ami" );
+            break;
+        case 4:
+        	System.out.println( "Ciao amico" );
+            break;
+    }
+    }
+}
